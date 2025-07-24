dc9d4fe89d71ef700853a6784060407bb1d72ab8
Chaosminecraft <volkswagenfelge@gmail.com>
2025-07-24 16:31:23 +0200

    modified:   TextConverter.py 	modified:   betaversion.txt

--- TextConverter.py
+++ TextConverter.py
@@ -4,7 +4,7 @@ import getpass, os, platform, socket, json, traceback, locale, time, datetime, t
 class version:
     release=False        #if that version is a release or beta version
     version="3.1.0"      #The release version
-    beta_version="3.1.6" #The Beta version
+    beta_version="3.1.7" #The Beta version
 
 #The settings variables in a class (Some name clashing was making me name the settings class to config)
 class config:
